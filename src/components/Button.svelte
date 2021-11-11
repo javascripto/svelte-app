c845c0507643f96886a67d12a08f7e60056b7731
Yuri Almeida <yurialves2@gmail.com>
2021-11-11 19:53:35 -0300

    chore: create button

---
+++ src/components/Button.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+
+export let text = '';
+export let double = false;
+export let triple = false;
+
+</script>
+
+<button class="button" class:triple={triple} class:double>
+  {text}
+</button>
+
+<style>
+  .button {
+    flex-basis: 25%;
+  }
+  .double {
+    flex-basis: 50%;
+  }
+  .triple {
+    flex-basis: 75%;
+  }
+</style>
