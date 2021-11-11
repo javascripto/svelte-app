d705c62c51ced64e3b3f947025800e6eb0f8d7fa
Yuri Almeida <yurialves2@gmail.com>
2021-11-11 19:53:56 -0300

    chore: create basic calculator layout

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,23 @@
 <script lang="ts">
-	export let name: string;
+	import Calculator from "./components/Calculator.svelte";
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<Calculator />
 </main>
 
 <style>
+	:global(body) {
+		background-color: black;
+	}
+
+	:root {
+		--bg-color: #555;
+	}
 	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 100vh;
 	}
 </style>
---
+++ src/components/Calculator.svelte
@@ -0,0 +1,44 @@
+<script>
+  import Line from "./Line.svelte";
+	import Button from "./Button.svelte";
+</script>
+
+<div class="calculator">
+  <Line>
+    <Button text="AC" triple />
+    <Button text="/" />
+  </Line>
+  <Line>
+    <Button text="7" />
+    <Button text="8" />
+    <Button text="9" />
+    <Button text="*" />
+  </Line>
+  <Line>
+    <Button text="4" />
+    <Button text="5" />
+    <Button text="6" />
+    <Button text="+" />
+  </Line>
+  <Line>
+    <Button text="1" />
+    <Button text="2" />
+    <Button text="3" />
+    <Button text="-" />
+  </Line>
+  <Line>
+    <Button text="0" double />
+    <Button text="," />
+    <Button text="=" />
+  </Line>
+</div>
+
+<style>
+  .calculator {
+    width: 150px;
+    height: 220px;
+    display: flex;
+    flex-direction: column;
+    background-color: var(--bg-color);
+  }
+</style>
