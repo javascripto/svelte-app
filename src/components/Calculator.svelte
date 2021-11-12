c81ca0c838a5d8e25b8fe7e8615518030ced8f87
Yuri Almeida <yurialves2@gmail.com>
2021-11-11 21:47:41 -0300

    chore: bind calculator view and model

--- src/components/Calculator.svelte
+++ src/components/Calculator.svelte
@@ -2,48 +2,50 @@
   import Line from "./Line.svelte";
 	import Button from "./Button.svelte";
   import Display from "./Display.svelte";
+  import CalculatorModel from "../models/Calculator";
 
-  let _value = '0';
-
-  function onClick(value: string) {
-    _value += value
-  }
-
+  let calc = new CalculatorModel();
+  const pressClear = () => calc = calc.clear()
+  const pressPoint = () => calc = calc.typePoint()
+  const pressEqual = () => calc = calc.calculate()
+  const pressNumber = (number: string) => calc = calc.typeNumber(number)
+  const pressOperator = (operator: string) => calc = calc.typeOperator(operator)
 </script>
 
 <div class="calculator">
-  <Display value={_value} />
+  <Display value={calc.value} />
   <Line>
-    <Button text="AC" triple highlight />
-    <Button text="/" operation />
+    <Button text="AC" triple highlight onClick={pressClear} />
+    <Button text="/" operation onClick={pressOperator} />
   </Line>
   <Line>
-    <Button text="7" onClick={onClick} />
-    <Button text="8" onClick={onClick} />
-    <Button text="9" onClick={onClick} />
-    <Button text="*" operation />
+    <Button text="7" onClick={pressNumber} />
+    <Button text="8" onClick={pressNumber} />
+    <Button text="9" onClick={pressNumber} />
+    <Button text="*" operation onClick={pressOperator} />
   </Line>
   <Line>
-    <Button text="4" onClick={onClick} />
-    <Button text="5" onClick={onClick} />
-    <Button text="6" onClick={onClick} />
-    <Button text="+" operation />
+    <Button text="4" onClick={pressNumber} />
+    <Button text="5" onClick={pressNumber} />
+    <Button text="6" onClick={pressNumber} />
+    <Button text="+" operation onClick={pressOperator} />
   </Line>
   <Line>
-    <Button text="1" onClick={onClick} />
-    <Button text="2" onClick={onClick} />
-    <Button text="3" onClick={onClick} />
-    <Button text="-" operation />
+    <Button text="1" onClick={pressNumber} />
+    <Button text="2" onClick={pressNumber} />
+    <Button text="3" onClick={pressNumber} />
+    <Button text="-" operation onClick={pressOperator} />
   </Line>
   <Line>
-    <Button text="0" double onClick={onClick} />
-    <Button text="," />
-    <Button text="=" highlight />
+    <Button text="0" double onClick={pressNumber} />
+    <Button text="," onClick={pressPoint} />
+    <Button text="=" highlight onClick={pressEqual} />
   </Line>
 </div>
 
 <style>
   .calculator {
+    zoom: 2;
     width: 150px;
     height: 220px;
     display: flex;
